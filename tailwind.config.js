3c453720ba0b33c596022ba16db7a0c7371f1f8e
Jeanette Yue <28907347+j-yue@users.noreply.github.com>
2023-09-19 09:36:28 -0700

    Extend theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,38 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          100: "#978FDF",
+          200: "#7A70C5",
+          300: "#6559B2",
+          400: "#53479B",
+        },
+        secondary: {
+          100: "#5FD9B7",
+          200: "#48C8A3",
+          300: "#23AC81",
+          400: "#0B8D63",
+        },
+        contessa: {
+          100: "#E09C90",
+          200: "#C27A6F",
+          300: "#AC6157",
+          400: "#924C44",
+        },
+        pearl: {
+          100: "#FFFAE9",
+          200: "#F9EFCB",
+          300: "#D9CB9F",
+          400: "#BAA874",
+        },
+      },
+      minWidth: {
+        xs: "320px",
+      },
+      padding: {
+        default: "var(--inner-padding)",
+      },
       screens: {
         xs: "320px",
         ...defaultTheme.screens,
