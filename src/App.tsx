c4b910fdb0619db0fd4409a9af338e74d5d84a8e
Jeanette Yue <28907347+j-yue@users.noreply.github.com>
2023-09-20 01:37:08 -0700

    Update App with all functionality and components implemented so far

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
+import { useState } from "react";
+import Banner from "./components/Banner";
+import Search from "./components/Search";
+import SearchResults from "./components/SearchResults";
+import { ApiDataType } from "./components/types";
+import useFetch from "./components/hooks/useFetch";
+import { DataContext } from "./context/DataContext";
+import { PaginateContext, PaginateDimensions } from "./context/PaginateContext";
+
 function App() {
+  const [url, setUrl] = useState<string>(
+    "https://api.spacexdata.com/v3/capsules"
+  );
+
+  const [data, setData] = useState<ApiDataType[]>([]);
+
+  const [dimensions, setDimensions] = useState<PaginateDimensions>({
+    currentPage: 1,
+    count: 5,
+  });
+
+  useFetch({ setData: setData });
+
   return (
-    <>
-      <header>
-        <h1>Page banner</h1>
-      </header>
-      <main>
-        <section>
-          <h2>Search form</h2>
-        </section>
-        <section>
-          <h2>Data grid</h2>
-        </section>
-      </main>
-    </>
+    <DataContext.Provider value={{ data, setData }}>
+      <PaginateContext.Provider value={{ dimensions, setDimensions }}>
+        <Banner />
+        <main>
+          <Search url={url} setUrl={setUrl} />
+          {data && <SearchResults />}
+        </main>
+      </PaginateContext.Provider>
+    </DataContext.Provider>
   );
 }
 
